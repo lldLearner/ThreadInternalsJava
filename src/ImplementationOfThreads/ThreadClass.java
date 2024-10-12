96b8a55761e21536490df303a02b114e8d718574
lldLearner <kscollab0067@gmail.com>
2024-10-12 11:18:19 +0530

    Thread Class With Default Constructor

---
+++ src/ImplementationOfThreads/ThreadClass.java
@@ -0,0 +1,9 @@
+package ImplementationOfThreads;
+
+public class ThreadClass extends Thread {
+
+	public static void main(String[] args) {
+		ThreadClass thread = new ThreadClass();
+		thread.start();
+	}
+}
